f8e1bc410eb1a18db1b114cc8f126f1359aee795
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-03-18 23:03:01 +0100

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,10 @@
 import css from './ContactForm.module.css';
 import { useId } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, useFormik } from 'formik';
 import { nanoid } from 'nanoid';
 import * as Yup from 'yup';
 import { FaArrowRight } from 'react-icons/fa';
+import clsx from 'clsx';
 
 const ContactForm = ({ addingContact }) => {
   const formId = {
@@ -19,12 +20,12 @@ const ContactForm = ({ addingContact }) => {
 
   const FeedbackSchema = Yup.object().shape({
     name: Yup.string()
-      .min(3, 'Too Short!')
-      .max(50, 'Too Long!')
+      .min(3, 'Too Short! Length must be between 3 and 50 characters')
+      .max(50, 'Too Long! Length must be between 3 and 50 characters')
       .required('Required'),
     number: Yup.string()
-      .min(3, 'Too Short!')
-      .max(50, 'Too Long!')
+      .min(3, 'Too Short! Length must be between 3 and 50 characters')
+      .max(50, 'Too Long! Length must be between 3 and 50 characters')
       .required('Required'),
   });
 
@@ -34,33 +35,43 @@ const ContactForm = ({ addingContact }) => {
       onSubmit={handleSubmit}
       validationSchema={FeedbackSchema}
     >
-      <Form className={css.form}>
-        <>
-          <label htmlFor={formId.name}>Name</label>{' '}
-          <Field
-            className={css.input}
-            type="text"
-            name="name"
-            id={formId.name}
-          />
-          <ErrorMessage className={css.error} name="name" as="span" />
-        </>
+      {({ errors, touched }) => (
+        <Form className={css.form}>
+          <>
+            <label htmlFor={formId.name}>Name</label>
+            <Field
+              className={clsx(css.input, {
+                [css.invalid]: errors.name && touched.name,
+              })}
+              type="text"
+              name="name"
+              id={formId.name}
+            />
+            <ErrorMessage name="name" className={css.error} component="span" />
+          </>
 
-        <>
-          <label htmlFor={formId.number}>Number</label>
-          <Field
-            className={css.input}
-            type="tel"
-            name="number"
-            id={formId.number}
-          />
-          <ErrorMessage className={css.error} name="number" as="span" />
-        </>
+          <>
+            <label htmlFor={formId.number}>Number</label>
+            <Field
+              className={clsx(css.input, {
+                [css.invalid]: errors.number && touched.number,
+              })}
+              type="tel"
+              name="number"
+              id={formId.number}
+            />
+            <ErrorMessage
+              name="number"
+              className={css.error}
+              component="span"
+            />
+          </>
 
-        <button className={css.addButton} type="submit">
-          <span>Add contact</span> <FaArrowRight className={css.icon} />
-        </button>
-      </Form>
+          <button className={css.addButton} type="submit">
+            <span>Add contact</span> <FaArrowRight className={css.icon} />
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
